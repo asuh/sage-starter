b54aecedc608bc992ef23493668f96d50ec1d2c4
Micah <asuh@users.noreply.github.com>
2016-12-24 14:05:06 -0700

    Error recovery for Gulp to prevent Gulp from crashing

--- gulpfile.js
+++ gulpfile.js
@@ -71,6 +71,12 @@ var enabled = {
 // Path to the compiled assets manifest in the dist directory
 var revManifest = path.dist + 'assets.json';
 
+// Error checking; produce an error rather than crashing.
+var onError = function (err) {
+	console.log(err.toString());
+	this.emit('end');
+};
+
 // ## Reusable Pipelines
 // See https://github.com/OverZealous/lazypipe
 
@@ -180,6 +186,7 @@ gulp.task('styles', ['wiredep'], function() {
       });
     }
     merged.add(gulp.src(dep.globs, {base: 'styles'})
+      .pipe(plumber({errorHandler: onError}))               
       .pipe(cssTasksInstance));
   });
   return merged
@@ -194,6 +201,7 @@ gulp.task('scripts', ['lint'], function() {
   manifest.forEachDependency('js', function(dep) {
     merged.add(
       gulp.src(dep.globs, {base: 'scripts'})
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(jsTasks(dep.name))
     );
   });

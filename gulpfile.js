5ea3680859f3cb08dc2daab04e8a02f522194150
Micah <asuh@users.noreply.github.com>
2017-01-10 15:58:44 -0800

    Update gulp watch for SVG sprites

--- gulpfile.js
+++ gulpfile.js
@@ -307,7 +307,7 @@ gulp.task('watch', function() {
   gulp.watch([path.source + 'scripts/**/*'], ['lint', 'scripts']);
   gulp.watch([path.source + 'fonts/**/*'], ['fonts']);
   gulp.watch([path.source + 'images/**/*'], ['images']);
-  gulp.watch([path.source + 'svg/**/*'], ['svg']);
+  gulp.watch([path.source + 'images/sprite/**/*'], ['svg']);
   gulp.watch(['bower.json', 'assets/manifest.json'], ['build']);
 });
 
